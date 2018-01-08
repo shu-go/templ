a54a09ab4faa6379e115b4a774649e1edd91c479
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2018-01-08 12:25:07 +0900

    change CLI framework to gli

--- cmd/templ/main.go
+++ cmd/templ/main.go
@@ -8,158 +8,84 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/urfave/cli"
+	"bitbucket.org/shu/gli"
 )
 
-func main() {
-	home := homePath()
-	if home == "" {
-		fmt.Fprintf(os.Stderr, "set env $TEMPL_HOME first.\n")
-		return
-	}
-	os.MkdirAll(home, os.ModeDir|0600)
-
-	app := cli.NewApp()
-	app.Name = "templ"
-	app.Commands = []cli.Command{
-		{
-			Name:      "generate",
-			Aliases:   []string{"gen"},
-			Usage:     "generate a template",
-			ArgsUsage: "{name}",
-			Flags:     []cli.Flag{
-			//cli.StringFlag{Name: "name, n", Usage: "name of the template"},
-			},
-			Action: func(c *cli.Context) error {
-				//name := c.String("name")
-				name := c.Args().First()
-
-				if name == "" {
-					fmt.Fprintf(os.Stderr, "missing name\n")
-					return nil
-				}
-
-				if err := runGenerate(home, name); err != nil {
-					fmt.Fprintf(os.Stderr, "%v\n", err)
-					return nil
-				}
-				return nil
-			},
-		},
-		{
-			Name:      "check",
-			Aliases:   []string{"chk", "test"},
-			Usage:     "check a template",
-			ArgsUsage: "{name}",
-			Flags:     []cli.Flag{
-			//cli.StringFlag{Name: "name, n", Usage: "name of the template"},
-			},
-			Action: func(c *cli.Context) error {
-				//name := c.String("name")
-				name := c.Args().First()
-
-				if name == "" {
-					fmt.Fprintf(os.Stderr, "missing name\n")
-					return nil
-				}
-
-				if err := runCheck(home, name); err != nil {
-					fmt.Fprintf(os.Stderr, "%v\n", err)
-					return nil
-				}
-				return nil
-			},
-		},
-		{
-			Name: "apply",
-			//Aliases: []string{"app"},
-			Usage:     "apply a template",
-			ArgsUsage: "{name} {target}",
-			Flags:     []cli.Flag{
-			//cli.StringFlag{Name: "name", Usage: "name of the template"},
-			//cli.StringFlag{Name: "target", Usage: "path of applied dir"},
-			},
-			Action: func(c *cli.Context) error {
-				//name := c.String("name")
-				//target := c.String("target")
-
-				name := c.Args().First()
-				target := c.Args().Get(1)
-
-				if name == "" {
-					fmt.Fprintf(os.Stderr, "missing name\n")
-					return nil
-				}
-				if target == "" {
-					target = "."
-				}
-				target, _ = filepath.Abs(target)
-
-				if err := runApply(home, name, target); err != nil {
-					fmt.Fprintf(os.Stderr, "%v\n", err)
-					return nil
-				}
-				return nil
-			},
-		},
-		{
-			Name:    "list",
-			Aliases: []string{"ls"},
-			Usage:   "list templates",
-			Flags: []cli.Flag{
-				cli.BoolFlag{Name: "verbose, v", Usage: "verbose output"},
-			},
-			Action: func(c *cli.Context) error {
-				verbose := c.Bool("verbose")
-				return runList(home, verbose)
-			},
-		},
-	}
-	app.Run(os.Args)
+type globalCmd struct {
+	Generate generateCmd `cli:"generate, gen"  help:"generate a template"  usage:"templ generate {TEMPL_NAME}"`
+	Check    checkCmd    `cli:"check, chk, test"  help:"check a template validity"  usage:"templ check {TEMPL_NAME}"`
+	Apply    applyCmd    `cli:"apply"  help:"apply a template"  usage:"templ apply {TEMPL_NAME} {DEST_DIR(default: .)}"`
+	List     listCmd     `cli:"list, ls"  help:"list templates"  usage:"templ list [-v]}"`
 }
 
-func runList(home string, verbose bool) error {
-	templs := []string{}
+type generateCmd struct{}
 
-	dirs, err := filepath.Glob(filepath.Join(home, "*"))
-	if err != nil {
-		return err
-	}
-	for _, v := range dirs {
-		if stat, err := os.Lstat(v); err != nil {
-			continue
-		} else if stat.IsDir() {
-			templs = append(templs, v)
-		}
+func (c generateCmd) Run(args []string) error {
+	var name string
+	if len(args) > 0 {
+		name = args[0]
 	}
 
-	for _, v := range templs {
-		fmt.Println("* " + filepath.Base(v))
-
-		t := NewTemplate(v)
-		if err := t.LoadDef(); err != nil {
-			fmt.Printf("  failed to load definitions: %s\n", err)
-			continue
-		}
-
-		if t.Def.Description != "" {
-			fmt.Println("  [Description]")
-			fmt.Printf("  %v\n", t.Def.Description)
-		}
-
-		// do something?
-		if verbose && len(t.Def.Vars) > 0 {
-			fmt.Println("  [Variables]")
-			for k, v := range t.Def.Vars {
-				fmt.Printf("  - %v:\t%T\n", k, v)
-			}
-		}
+	if name == "" {
+		fmt.Fprintf(os.Stderr, "missing name\n")
+		return nil
 	}
+
+	home := homePath()
+
+	templ := NewTemplate(filepath.Join(home, name))
+	fmt.Printf("generate `%s`...\n", templ.DefPath())
+
+	templ.Def.Author = "author"
+	templ.Def.Description = "description of this template."
+
+	// define sample variables
+	templ.Def.Vars["_SampleStrPrompted"] = ""
+	templ.Def.Vars["_SampleNumPrompted"] = 0
+	templ.Def.Vars["SampleStr"] = "value1"
+	templ.Def.Vars["SampleNum"] = 100
+	templ.Def.Vars["SampleList"] = []interface{}{"a", "b", 100}
+	templ.Def.Vars["SampleMap"] = map[string]interface{}{"key1": "a", "key2": 100}
+
+	if err := templ.Save(); err != nil {
+		return fmt.Errorf("save: %v", err)
+	}
+
+	// extras
+	os.MkdirAll(filepath.Join(home, name, "this_is_based_on"), os.ModeDir|0600)
+	ioutil.WriteFile(filepath.Join(home, name, "this_is_based_on", "file_{{ .SampleStr }}_{{ .SampleNum }}.txt"), []byte(`SampleList:
+{{ range .SampleList }}
+{{- "" }}  - {{ . }}
+{{ end }}
+
+Map:
+{{ range $key, $value := .SampleMap }}
+{{- "" }}  {{ $key }} : {{ $value }}
+{{ end }}
+
+Prompted:
+  - _SampleStrPrompted : {{ ._SampleStrPrompted }}
+  - _SampleNumPrompted : {{ ._SampleNumPrompted }}
+`), 0600)
 
 	return nil
 }
 
-func runCheck(home, name string) error {
+type checkCmd struct{}
+
+func (c checkCmd) Run(args []string) error {
+	var name string
+	if len(args) > 0 {
+		name = args[0]
+	}
+
+	if name == "" {
+		fmt.Fprintf(os.Stderr, "missing name\n")
+		return nil
+	}
+
+	home := homePath()
+
 	templ := NewTemplate(filepath.Join(home, name))
 	fmt.Printf("check `%s`...\n", templ.Path)
 
@@ -185,7 +111,30 @@ func runCheck(home, name string) error {
 
 	return nil
 }
-func runApply(home, name, target string) error {
+
+type applyCmd struct{}
+
+func (c applyCmd) Run(args []string) error {
+	var name string
+	if len(args) > 0 {
+		name = args[0]
+	}
+
+	if name == "" {
+		fmt.Fprintf(os.Stderr, "missing name\n")
+		return nil
+	}
+
+	var target string
+	if len(args) > 1 {
+		target = args[1]
+	} else {
+		target = "."
+	}
+	target, _ = filepath.Abs(target)
+
+	home := homePath()
+
 	templ := NewTemplate(filepath.Join(home, name))
 	fmt.Printf("apply `%s`...\n", templ.Path)
 
@@ -228,41 +177,71 @@ func runApply(home, name, target string) error {
 	return nil
 }
 
-func runGenerate(home, name string) error {
-	templ := NewTemplate(filepath.Join(home, name))
-	fmt.Printf("generate `%s`...\n", templ.DefPath())
+type listCmd struct {
+	Verbose bool `cli:"verbose, v"  help:"verbose output"`
+}
 
-	templ.Def.Author = "author"
-	templ.Def.Description = "description of this template."
+func (c listCmd) Run(args []string) error {
+	templs := []string{}
 
-	// define sample variables
-	templ.Def.Vars["_SampleStrPrompted"] = ""
-	templ.Def.Vars["_SampleNumPrompted"] = 0
-	templ.Def.Vars["SampleStr"] = "value1"
-	templ.Def.Vars["SampleNum"] = 100
-	templ.Def.Vars["SampleList"] = []interface{}{"a", "b", 100}
-	templ.Def.Vars["SampleMap"] = map[string]interface{}{"key1": "a", "key2": 100}
+	home := homePath()
 
-	if err := templ.Save(); err != nil {
-		return fmt.Errorf("save: %v", err)
+	dirs, err := filepath.Glob(filepath.Join(home, "*"))
+	if err != nil {
+		return err
+	}
+	for _, v := range dirs {
+		if stat, err := os.Lstat(v); err != nil {
+			continue
+		} else if stat.IsDir() {
+			templs = append(templs, v)
+		}
 	}
 
-	// extras
-	os.MkdirAll(filepath.Join(home, name, "this_is_based_on"), os.ModeDir|0600)
-	ioutil.WriteFile(filepath.Join(home, name, "this_is_based_on", "file_{{ .SampleStr }}_{{ .SampleNum }}.txt"), []byte(`SampleList:
-{{ range .SampleList }}
-{{- "" }}  - {{ . }}
-{{ end }}
+	for _, v := range templs {
+		fmt.Println("* " + filepath.Base(v))
 
-Map:
-{{ range $key, $value := .SampleMap }}
-{{- "" }}  {{ $key }} : {{ $value }}
-{{ end }}
+		t := NewTemplate(v)
+		if err := t.LoadDef(); err != nil {
+			fmt.Printf("  failed to load definitions: %s\n", err)
+			continue
+		}
 
-Prompted:
-  - _SampleStrPrompted : {{ ._SampleStrPrompted }}
-  - _SampleNumPrompted : {{ ._SampleNumPrompted }}
-`), 0600)
+		if t.Def.Description != "" {
+			fmt.Println("  [Description]")
+			fmt.Printf("  %v\n", t.Def.Description)
+		}
+
+		// do something?
+		if c.Verbose && len(t.Def.Vars) > 0 {
+			fmt.Println("  [Variables]")
+			for k, v := range t.Def.Vars {
+				fmt.Printf("  - %v:\t%T\n", k, v)
+			}
+		}
+	}
 
 	return nil
+}
+
+func main() {
+	home := homePath()
+	if home == "" {
+		fmt.Fprintf(os.Stderr, "set env $TEMPL_HOME first.\n")
+		return
+	}
+	os.MkdirAll(home, os.ModeDir|0600)
+
+	app := gli.New(&globalCmd{})
+	app.Name = "templ"
+	app.Desc = "file templater"
+	app.Version = "0.2.0"
+	app.Usage = `set env $TEMPL_HOME as a template repository(storage).
+then, templ gen {your template name here}
+then, cd to where to apply the template
+then, templ apply {the template name}`
+	err := app.Run(os.Args)
+	if err != nil {
+		os.Exit(1)
+	}
 }
